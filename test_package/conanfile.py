60574ebd405ea6e5b2a2b03c0f6e13c1855f3c6a
Uilian Ries <uilianries@gmail.com>
2018-11-07 12:02:01 -0200

    Include GYP test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,9 +4,26 @@
 from conans import ConanFile
 
 
+TEST_PACKAGE_GYP = """{
+    'targets': [
+      {
+        'target_name': 'test_package',
+        'type': 'executable',
+        'sources': [
+          'test_package.cpp',
+        ],
+      },
+    ],
+}
+"""
+
+
 class TestPackage(ConanFile):
 
     def test(self):
         self.run("gyp --help")
         # ensure python can import gyp
         self.run('python -c "from __future__ import print_function;import gyp;print(gyp)"')
+        with open('test_package.gyp', 'w') as fd:
+            fd.write(TEST_PACKAGE_GYP)
+        self.run("gyp  --depth=. test_package.gyp")

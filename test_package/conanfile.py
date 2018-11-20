0ed138c5786403696ad6461027611a884d3f52b3
Uilian Ries <uilianries@gmail.com>
2018-11-20 12:49:26 -0200

    Fix msvc and xcode generators
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import shutil
 from conans import ConanFile
 
 
 TEST_PACKAGE_GYP = """{
-    'targets': [
+    "targets": [
       {
-        'target_name': 'test_package',
-        'type': 'executable',
-        'sources': [
-          'test_package.cpp',
-        ],
-      },
-    ],
+        "target_name": "test_package",
+        "type": "executable",
+        "sources": [
+          "test_package.cpp"
+        ]
+      }
+    ]
 }
 """
-
 
 class TestPackage(ConanFile):
 
@@ -24,6 +25,9 @@ class TestPackage(ConanFile):
         self.run("gyp --help")
         # ensure python can import gyp
         self.run('python -c "from __future__ import print_function;import gyp;print(gyp)"')
-        with open('test_package.gyp', 'w') as fd:
+        with open("test_package.gyp", "w") as fd:
             fd.write(TEST_PACKAGE_GYP)
-        self.run("gyp  --depth=. test_package.gyp")
+        shutil.copyfile(os.path.join(self.source_folder, "test_package.cpp"), os.path.join(os.getcwd(), "test_package.cpp"))
+        self.run("gyp test_package.gyp --depth=. -f make --generator-output=%s" % os.path.join("build", "makefiles"))
+        self.run("gyp test_package.gyp --depth=. -f xcode --generator-output=%s" % os.path.join("build", "xcodefiles"))
+        self.run("gyp test_package.gyp --depth=. -f msvs -G msvs_version=2015")

15b6fcd47c12d539a1e85ecd2dda1f595a2a9cff
Uilian Ries <uilianries@gmail.com>
2018-12-28 12:44:21 -0200

    Build gyp installer on Centos
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bincrafters import build_template_header_only
+import os
+from bincrafters import build_template_installer
+from bincrafters import build_shared
+
 
 if __name__ == "__main__":
-    builder = build_template_header_only.get_builder()
+
+    arch = os.getenv("ARCH")
+    builder = build_template_installer.get_builder()
+    builder.add({"os" : build_shared.get_os(), "arch_build" : arch}, {}, {}, {})
     builder.run()
